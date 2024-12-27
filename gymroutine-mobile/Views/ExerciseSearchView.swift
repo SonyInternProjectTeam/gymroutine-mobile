2c85a5bcfdc104d667c29411000b5d3ce5efc236
yuki035 <yukiyuki1918@icloud.com>
2024-12-28 00:16:05 +0900

    refactor: ExerciseSearchView

--- gymroutine-mobile/Views/ExerciseSearchView.swift
+++ gymroutine-mobile/Views/ExerciseSearchView.swift
@@ -8,84 +8,82 @@
 import SwiftUI
 
 struct ExerciseSearchView: View {
+
     @ObservedObject var viewModel = ExerciseViewModel()
-    var selecttrainOptions : [String] = []
-    @State private var searchText = ""
-    @State private var contentHeight: CGFloat = 0.0
-    
-    private let Excercisecolumns = [
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
+
     private let Categorycolumns = [
         GridItem(.flexible()),
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
-    
+
+    private let Excercisecolumns = [
+        GridItem(.flexible(), spacing: 12),
+        GridItem(.flexible())
+    ]
+
     var body: some View {
-        NavigationStack {
-            VStack(spacing: 20){
+        ScrollView {
+            VStack(spacing: 20) {
                 ExerciseSearchField(text:$viewModel.searchWord)
                     .onSubmit {
                         viewModel.searchExerciseName(for: viewModel.searchWord)
                     }
+
                 CategoryView
-                ExeriseTabView
-            }
-            .padding([.top, .horizontal], 24)
-            .background(.gray.opacity(0.2))
-            .onAppear {
-                viewModel.fetchAll()
+
+                ExerciseGridView
             }
         }
+        .padding([.top, .horizontal], 24)
+        .background(.gray.opacity(0.2))
+        .onAppear {
+            viewModel.fetchAll()
+        }
     }
-    
+
     private var CategoryView: some View {
-        VStack (alignment: .center, spacing:40){
-            VStack(alignment: .leading, spacing: 12) {
-                Text("カテゴリ")
-                    .fontWeight(.bold)
-                    .hAlign(.leading)
-                    .font(.title2)
-                    .fontWeight(.bold)
-                LazyVGrid(columns: Categorycolumns,spacing: 20) {
-                    ForEach(ExercisePart.allCases, id: \.self) { part in
-                        ExercisePartToggle(flag:viewModel.selectedExerciseParts.contains(part),exercisePart: part)
-                            .onTapGesture {
-                                viewModel.onTapExercisePartToggle(part: part)
-                            }
+        VStack(alignment: .leading, spacing: 16) {
+            Text("カテゴリ")
+                .font(.title2)
+                .fontWeight(.bold)
+                .hAlign(.leading)
+
+            LazyVGrid(columns: Categorycolumns,spacing: 16) {
+                ForEach(ExercisePart.allCases, id: \.self) { part in
+                    Button {
+                        viewModel.onTapExercisePartToggle(part: part)
+
+                    } label: {
+                        ExercisePartToggle(flag:viewModel.selectedExerciseParts.contains(part),
+                                           exercisePart: part)
                     }
                 }
             }
         }
     }
-    
-    
-    private var ExeriseTabView: some View {
-        VStack (alignment: .center, spacing:40){
-            VStack(alignment: .leading, spacing: 12) {
-                Text("おすすめ")
-                    .fontWeight(.bold)
-                    .hAlign(.leading)
-                    .font(.title2)
-                    .fontWeight(.bold)
-            }
-            ScrollView {
-                LazyVGrid(columns: Excercisecolumns, spacing: 8) {
-                    ForEach(viewModel.filterExercises, id: \.self) { exercise in
-                        NavigationLink (destination: ExerciseDetailView(exercise: exercise),label: {
-                            ExersiceSelectButton(name:exercise.name, option: exercise.part)
-                        })
-                    }
+
+    private var ExerciseGridView: some View {
+        VStack (alignment: .leading, spacing: 16) {
+            Text("おすすめ")
+                .font(.title2)
+                .fontWeight(.bold)
+                .hAlign(.leading)
+
+            LazyVGrid(columns: Excercisecolumns, spacing: 12) {
+                ForEach(viewModel.filterExercises, id: \.self) { exercise in
+                    NavigationLink (destination: ExerciseDetailView(exercise: exercise), label: {
+                        ExersiceSelectButton(name:exercise.name, option: exercise.part)
+                    })
                 }
             }
+
         }
     }
 }
 
-
-
 #Preview {
-    ExerciseSearchView()
+    NavigationStack {
+        ExerciseSearchView()
+    }
 }
