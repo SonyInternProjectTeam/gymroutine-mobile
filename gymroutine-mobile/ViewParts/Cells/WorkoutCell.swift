ed380ac3ae2e9612b9f8a32abbd6aaab23471283
sattsat <syuuatit141offer@gmail.com>
2025-01-09 01:50:53 +0900

    design: workoutのカスタムセルのレイアウトを構成

--- gymroutine-mobile/ViewParts/Cells/WorkoutCell.swift
+++ gymroutine-mobile/ViewParts/Cells/WorkoutCell.swift
@@ -9,11 +9,59 @@
 import SwiftUI
 
 struct WorkoutCell: View {
+    
+    let workoutName: String
+    let count: Int
+    let profileImageUrl: String?
+    
+    //workoutモデルがないため仮の変数を宣言
+    init() {
+        self.workoutName = "一軍ワークアウト"
+        self.count = 6
+        self.profileImageUrl = nil
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Group {
+                if let profileImageUrl {
+                    //URLが存在したら、URLから画像を取得するKingFisherなどを導入してここに追加
+                    Text("URLから画像処理")
+                } else {
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(.main)
+                        .frame(width: 48, height: 48)
+                }
+            }
+            
+            VStack(alignment: .leading) {
+                Text("\(count)種目")
+                    .foregroundStyle(.secondary)
+                    .font(.caption)
+                
+                Text(workoutName)
+                    .font(.headline)
+                    .lineLimit(1)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Text("〉")
+                .foregroundStyle(.secondary)
+                .bold()
+        }
+        .padding(8)
+        .background(.background)
+        .clipShape(.rect(cornerRadius: 8))
     }
 }
 
 #Preview {
-    WorkoutCell()
+    ScrollView {
+        WorkoutCell()
+        WorkoutCell()
+        WorkoutCell()
+    }
+    .padding()
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(.secondary)
 }
