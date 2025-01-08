09d8b756a9f11af6e280256d0d4e3e1630a98142
sattsat <syuuatit141offer@gmail.com>
2025-01-09 01:18:11 +0900

    add: workoutの一覧表示を担当するためのカスタムセルを作成

---
+++ gymroutine-mobile/ViewParts/Cells/WorkoutCell.swift
@@ -0,0 +1,19 @@
+//
+//  WorkoutCell.swift
+//  gymroutine-mobile
+//  
+//  Created by SATTSAT on 2025/01/09
+//  
+//
+
+import SwiftUI
+
+struct WorkoutCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutCell()
+}
