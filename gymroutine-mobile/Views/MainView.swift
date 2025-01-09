3dabfbe72724187c466b90190f2ae4eae809c319
sattsat <syuuatit141offer@gmail.com>
2025-01-09 19:41:34 +0900

    design: 今日のワークアウト一覧をMainViewに仮で追加

--- gymroutine-mobile/Views/MainView.swift
+++ gymroutine-mobile/Views/MainView.swift
@@ -13,21 +13,40 @@ struct MainView: View {
     @EnvironmentObject var userManager: UserManager
     
     var body: some View {
-        VStack {
-            
-                let email = userManager.currentUser?.email
+        ScrollView(showsIndicators: false) {
+            VStack {
                 
-                Text("\(email)でログインしました！")
-                    .font(.headline)
+                    let email = userManager.currentUser?.email
+                    
+                    Text("\(email)でログインしました！")
+                        .font(.headline)
+                    
                 
-            
 
-            Button {
-                viewModel.logout()
-            } label: {
-                Text("ログアウトするよ")
+                Button {
+                    viewModel.logout()
+                } label: {
+                    Text("ログアウトするよ")
+                }
+                
+                Text("今日のワークアウト")
+                    .font(.title2.bold())
+                    .hAlign(.leading)
+                
+                //仮表示
+                ForEach(0..<2) {_ in
+                    WorkoutCell()
+                }
+                
             }
         }
+        .padding()
+        .background(Color.gray.opacity(0.1))
     }
+}
+
+#Preview {
+    MainView(viewModel: MainViewModel(router: Router()))
+        .environmentObject(UserManager.shared)
 }
 
