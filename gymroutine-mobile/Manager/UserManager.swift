e783fbedcc1a34add51375b32a777edbc65f67bb
CHO SUNGHWA <63436365+hwacho9@users.noreply.github.com>
2025-02-02 21:52:20 +0900

    Update gymroutine-mobile/Manager/UserManager.swift
    Co-authored-by: sattsat-tdu <syuuatit141offer@gmail.com>

--- gymroutine-mobile/Manager/UserManager.swift
+++ gymroutine-mobile/Manager/UserManager.swift
@@ -55,15 +55,25 @@ class UserManager: ObservableObject {
     }
     
     // follower
-    func fetchFollowersCount(userId: String) async throws -> Int {
-        let snapshot = try await db.collection("Users").document(userId).collection("Followers").getDocuments()
-        return snapshot.documents.count
+    func fetchFollowersCount(userId: String) async -> Int {
+        do {
+            let snapshot = try await db.collection("Users").document(userId).collection("Followers").getDocuments()
+            return snapshot.documents.count
+        } catch {
+            print("[ERROR] フォロワー数の取得に失敗。id: \(userId)")
+            return -1
+        }
     }
     
     // following
-    func fetchFollowingCount(userId: String) async throws -> Int {
-        let snapshot = try await db.collection("Users").document(userId).collection("Following").getDocuments()
-        return snapshot.documents.count
+    func fetchFollowingCount(userId: String) async -> Int {
+        do {
+            let snapshot = try await db.collection("Users").document(userId).collection("Following").getDocuments()
+            return snapshot.documents.count
+        } catch {
+            print("[ERROR] フォロー数の取得に失敗。id: \(userId)")
+            return -1
+        }
     }
 }
 
