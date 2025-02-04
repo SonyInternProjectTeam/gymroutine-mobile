b77ae948537ac3874406f10a0894e1bbfd4c5c33
sattsat <syuuatit141offer@gmail.com>
2025-02-05 08:55:26 +0900

    add: 配列を指定の数で分割するExtensionを追加

---
+++ gymroutine-mobile/Extensions/Array+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Array+Extensions.swift
+//  gymroutine-mobile
+//  
+//  Created by SATTSAT on 2025/02/05
+//  
+//
+
+extension Array {
+    //配列を分割する・例：[1,2,3,4,5,6](sizeが2だった場合) -> [[1,2],[3,4],[5.6]]
+    func chunked(into size: Int) -> [[Element]] {
+        stride(from: 0, to: count, by: size).map {
+            Array(self[$0..<Swift.min($0 + size, count)])
+        }
+    }
+}
