8d980f5eed2783b1657208dd2e82d3cea63b8753
sattsat <syuuatit141offer@gmail.com>
2025-02-05 12:48:31 +0900

    feat: カレンダーを表示するViewの構成

--- gymroutine-mobile/Views/CalendarView.swift
+++ gymroutine-mobile/Views/CalendarView.swift
@@ -13,37 +13,130 @@ import SwiftUI
 struct CalendarView: View {
     
     @StateObject private var viewModel = CalendarViewModel()
+    @State private var routineFlg = false
+    
+    private let weekdays = ["日", "月", "火", "水", "木", "金", "土"]
+    
     var body: some View {
-        VStack {
-            Text("カレンダー")
-                .font(.title)
-                .padding()
+        VStack(spacing: 0) {
+            headerBox()
+            
+            calendarBox
+            
+            contentBox
+        }
+        .background(Color.gray.opacity(0.1))
+        .sheet(isPresented: $routineFlg) {
+            Text("ルーティーン追加画面を\nここに作成")
+        }
+    }
+}
 
-            List {
-                ForEach(generateDatesForCurrentMonth(), id: \.self) { date in
-                    Text(formatDate(date))
+//MARK: Views
+extension CalendarView {
+    @ViewBuilder
+    private func headerBox() -> some View {
+        if let selectedMonth = viewModel.selectedMonth {
+            Text(selectedMonth.toYearMonthString())
+                .font(.title2.bold())
+                .padding()
+                .hAlign(.center)
+                .overlay(alignment: .trailing) {
+                    Button(action: {
+                        routineFlg.toggle()
+                    }, label: {
+                        Image(systemName: "plus")
+                            .imageScale(.large).bold()
+                            .padding(.trailing)
+                    })
+                }
+        }
+    }
+    
+    private var calendarBox: some View {
+        VStack {
+            
+            LazyVGrid(columns: Array(repeating: GridItem(), count: 7)) {
+                ForEach(weekdays, id: \.self) { weekday in
+                    Text(weekday)
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            
+            Divider()
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 0) {
+                    ForEach(viewModel.months, id: \.self) { month in
+                        CalendarGridView(month.generateCalendarDays())
+                            .containerRelativeFrame([.horizontal, .vertical])
+                            .id(month)
+                    }
+                }
+                .scrollTargetLayout()
+            }
+            .scrollTargetBehavior(.paging)
+            .scrollPosition(id: $viewModel.selectedMonth)
+            .vAlign(.center)
+            .onChange(of: viewModel.selectedMonth) {
+                viewModel.onChangeMonth(viewModel.selectedMonth)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func CalendarGridView(_ days: [Date?]) -> some View {
+        VStack(spacing: 4) {
+            ForEach(Array(days.chunked(into: 7)), id: \.self) { week in
+                HStack(spacing: 4) {
+                    ForEach(0..<7, id: \.self) { index in
+                        Group {
+                            if index < week.count, let validDay = week[index] {
+                                Text("\(Calendar.current.component(.day, from: validDay))")
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 4)
+                                            .stroke(validDay.isSameDay(as: viewModel.selectedDate) ? .main : .clear, lineWidth: 2)
+                                    )
+                                    .onTapGesture {
+                                        viewModel.selectedDate = validDay
+                                    }
+                            } else {
+                                Rectangle()
+                                    .fill(Color.clear)
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            }
+                        }
+                        .padding(2)
+                    }
                 }
             }
         }
-        .navigationTitle("Calendar")
+        .vAlign(.center)
     }
-
-    private func generateDatesForCurrentMonth() -> [Date] {
-        let calendar = Calendar.current
-        let today = Date()
-        let range = calendar.range(of: .day, in: .month, for: today)!
-
-        return range.compactMap { day -> Date? in
-            var components = calendar.dateComponents([.year, .month], from: today)
-            components.day = day
-            return calendar.date(from: components)
+    
+    private var contentBox: some View {
+        VStack {
+            Text(viewModel.selectedDate.toMonthDayWeekdayString())
+                .font(.headline)
+                .padding(.horizontal, 24)
+                .padding(.vertical, 12)
+                .hAlign(.leading)
+                .background(Color.gray.opacity(0.2))
+            
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    Text("ここにWorkoutCellを配置")
+                    Text("ここにWorkoutCellを配置")
+                    Text("ここにWorkoutCellを配置")
+                }
+            }
+            .vAlign(.top)
         }
     }
+}
 
-    private func formatDate(_ date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .long
-        formatter.locale = Locale(identifier: "ja_JP")
-        return formatter.string(from: date)
-    }
+#Preview {
+    CalendarView()
 }
