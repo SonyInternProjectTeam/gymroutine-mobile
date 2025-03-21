cba3bedfcd57fb28a609829034247d8e4a63e166
sattsat <syuuatit141offer@gmail.com>
2025-03-21 09:30:15 +0900

    feat: 画面上部にメッセージを一定時間表示するバナーの実装

--- gymroutine-mobile/Extensions/UIApplication+Extensions.swift
+++ gymroutine-mobile/Extensions/UIApplication+Extensions.swift
@@ -13,6 +13,7 @@ import SwiftUI
 extension UIApplication {
     
     static var loadingWindow: UIWindow?
+    static var bannerWindow: UIWindow?
     
     //ローディングViewの表示
     static func showLoading(message: String? = nil) {
@@ -29,7 +30,47 @@ extension UIApplication {
         newWindow.makeKeyAndVisible()
     }
     
+    // ローディングViewの非表示
     static func hideLoading() {
         loadingWindow = nil
     }
+    
+    static func showBanner(type: BannerType, message: String) {
+        guard let windowScene = UIApplication.shared.connectedScenes
+            .first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene else { return }
+        
+        let newWindow = TransparentTouchWindow(windowScene: windowScene)
+        let vc = UIHostingController(rootView: BannerView(type: type, message: message))
+        
+        vc.view.backgroundColor = .clear
+        newWindow.rootViewController = vc
+        newWindow.windowLevel = UIWindow.Level.alert + 1
+        UIApplication.loadingWindow = newWindow
+        newWindow.makeKeyAndVisible()
+    }
 }
+
+// View表示部分以外のタッチを有効可するカスタムUIWindow
+final class TransparentTouchWindow: UIWindow {
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        guard let hitView = super.hitTest(point, with: event),
+              let rootView = rootViewController?.view else {
+                  return nil
+              }
+        
+        if #available(iOS 18, *) {
+            for subView in rootView.subviews.reversed() {
+                let pointInSubView = subView.convert(point, from: rootView)
+                if subView.hitTest(pointInSubView, with: event) == subView {
+                    return hitView
+                }
+            }
+            
+            return nil
+        } else {
+            return hitView == rootView ? nil : hitView
+        }
+    }
+}
+
