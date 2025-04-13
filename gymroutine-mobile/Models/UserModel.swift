6232e04fb88ed8a63f3b74e689873213917d9700
hwacho9 <csh77776@gmail.com>
2025-04-13 18:05:57 +0900

    fix:merge usermodel

--- gymroutine-mobile/Models/UserModel.swift
+++ gymroutine-mobile/Models/UserModel.swift
@@ -18,15 +18,6 @@ struct User: Decodable, Identifiable {
     var birthday: Date? = nil // birthday
     var gender: String = "" // gender
     var createdAt: Date = Date()
-<<<<<<< HEAD
-    
-    /// 年齢を計算する
-    var age: Int {
-        guard let birthday = birthday else { return 0 }
-        let calendar = Calendar.current
-        let ageComponents = calendar.dateComponents([.year], from: birthday, to: Date())
-        return ageComponents.year ?? 0
-=======
 
     init(uid: String, email: String, name: String = "", profilePhoto: String = "", visibility: Int = 2, isActive: Bool = false, birthday: Date? = nil, gender: String = "", createdAt: Date = Date()) {
         self.uid = uid
@@ -38,6 +29,5 @@ struct User: Decodable, Identifiable {
         self.birthday = birthday
         self.gender = gender
         self.createdAt = createdAt
->>>>>>> SP9-3Workout
     }
 }
